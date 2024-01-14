0655b1464d25d0bc1a189e5f947c93e1b391095c
thefutureseer <danielharrislimos@outlook.com>
2024-01-13 23:53:29 -0800

    add a way for user to post to the readme

---
+++ public/js/javascript.js
@@ -0,0 +1,33 @@
+// Fetch the content of the Markdown file
+function fetchReadme() {    
+ fetch('README.md')
+  .then(response => response.text())
+   .then(markdown => {
+        // Convert Markdown to HTML using marked
+        document.getElementById('markdown-content').innerHTML = marked.parse(markdown);})
+    .catch(error => console.error('Error fetching README.md:', error));
+};
+
+fetchReadme();
+
+// Add content to README.md
+function addText() {
+    const userInput = prompt('Enter text to add to README.md:');
+    if (userInput) {
+        // Send the user input to the server
+        fetch('http://localhost:8080/addText', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text: userInput }),
+        })
+            .then(response => response.text())
+            .then(result => {
+                console.log(result)
+             //after adding then show it on the page:
+             fetchReadme();
+            })
+            .catch(error => console.error('Error:', error));
+    }
+}
